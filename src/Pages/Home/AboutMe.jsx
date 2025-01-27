588fa131930973205ffc7bc33207c7ffd7cbb287
bekyamen <bereket1terefe@gmail.com>
2025-01-27 10:44:06 +0300

    upload projectfile

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,26 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.jpg" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+          Hi, I'm Bereket, a dedicated full-stack developer from Ethiopia with a passion for building dynamic and 
+          responsive web applications using the MERN stack. Driven by curiosity and a commitment to continuous 
+          learning, 
+          I stay updated with the latest technologies to deliver modern and efficient solutions.
+          </p>
+          <p className="hero--section-description">
+          I thrive on contributing to innovative projects and collaborating with like-minded professionals to 
+          create impactful digital experiences. 
+          Welcome to my portfolio—let’s bring ideas to life together!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Bereket</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+            I build modern websites with the latest technologies.
+            <br /> 
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.jpg" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
